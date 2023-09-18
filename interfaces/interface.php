0c2bb3c0153d672c95d45bc7ead54a1b35f6e985
Kanusan Karunarajah <kanusan1@hotmail.ch>
2023-09-18 11:47:42 +0200

    countable interface

--- interfaces/interface.php
+++ interfaces/interface.php
@@ -7,13 +7,17 @@ interface LogInterface {
     public function log($message);
 }
 
-class Table implements TableInterface, LogInterface {
+class Table implements TableInterface, LogInterface, Countable {
     public function save(array $data) {
         return 'foo';
     }
     public function log($message) {
         return $message . "\n";
     }
+
+    public function count() {
+        return 10;
+    }
 }
 echo (new Table())->save([]);
-echo (new Table())->log(' funktionierts?');
+echo (new Table())->count();
